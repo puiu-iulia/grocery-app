5ffb0dafdbe85b203f70f228c2b1a781ec5f9ef3
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-08-21 15:04:41 +0300

    Clean up code

--- app/index.tsx
+++ app/index.tsx
@@ -8,141 +8,126 @@ import { GroceryListItem} from '@/components/GroceryItem';
 
 export default function HomeScreen() {
 
-    const realm = useRealm();
-    const user = useUser();
+  const realm = useRealm();
+  const user = useUser();
 
-    const [groceryName, setGroceryName] = useState('');
+  const [groceryName, setGroceryName] = useState('');
 
-    const createGroceryItem = () => {
-        realm.write(() => {
-            realm.create(GroceryItem, {
-                name: groceryName,
-                user_id: user.id,
-            });
+  const createGroceryItem = () => {
+    realm.write(() => {
+        realm.create(GroceryItem, {
+            name: groceryName,
+            user_id: user.id,
         });
-        setGroceryName('');
-    }
+    });
+    setGroceryName('');
+  }
 
   const updateGroceryItem = (item: GroceryItem) => {
-      realm.write(() => {
-        item.isPurchased = !item.isPurchased;
-      });
+    realm.write(() => {
+      item.isPurchased = !item.isPurchased;
+    });
   }
 
   const deleteGroceryItem = (item: GroceryItem) => {
-      realm.write(() => {
-        realm.delete(item);
-      });
+    realm.write(() => {
+      realm.delete(item);
+    });
   }
 
   const groceryItems = useQuery(GroceryItem).filtered('user_id = $0', user.id).sorted('createdAt', true);
 
   return (
-        <View style={styles.screen}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>My Groceries</Text>
-            </View>
-            <View style={styles.groceryContainer}>
-                <TextInput 
-                  placeholder="Item Name"
-                  value={groceryName}
-                  onChangeText={setGroceryName}
-                  style={styles.nameContainer}
-                  onSubmitEditing={createGroceryItem}
-                />
-                <Pressable style={styles.plusIcon} onPress={createGroceryItem} disabled={!groceryName}>
-                    <FontAwesome6 name="basket-shopping" size={32} color={"#C1FF72"} />
-                </Pressable>
-            </View>
-            <SectionList 
-                sections={[
-                    {
-                      title: "Items to Purchase",
-                      data: groceryItems.filter((item) => !item.isPurchased)
-                    },
-                      {
-                      title: "Purchased Items",
-                      data: groceryItems.filter((item) => item.isPurchased)
-                    }
-                ]}
-                renderItem={({ item }) => (
-                    <GroceryListItem 
-                      item={item} 
-                      onDelete={() => deleteGroceryItem(item)}
-                      onPress={() => updateGroceryItem(item)}
-                    />
-                )}
-                renderSectionHeader={({section: {title}}) => (
-                    <Text style={styles.sectionHeader}>{title}</Text>
-                )}
-                keyExtractor={(item) => item._id.toString()}
-            />
+      <View style={styles.screen}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>My Groceries</Text>
         </View>
-    );
+        <View style={styles.groceryContainer}>
+          <TextInput 
+            placeholder="Item Name"
+            value={groceryName}
+            onChangeText={setGroceryName}
+            style={styles.nameContainer}
+            onSubmitEditing={createGroceryItem}
+          />
+          <Pressable style={styles.plusIcon} onPress={createGroceryItem} disabled={!groceryName}>
+            <FontAwesome6 name="basket-shopping" size={32} color={"#C1FF72"} />
+          </Pressable>
+        </View>
+        <SectionList 
+          sections={[
+            {
+              title: "Items to Purchase",
+              data: groceryItems.filter((item) => !item.isPurchased)
+            },
+            {
+              title: "Purchased Items",
+              data: groceryItems.filter((item) => item.isPurchased)
+            }
+          ]}
+          renderItem={({ item }) => (
+            <GroceryListItem 
+              item={item} 
+              onDelete={() => deleteGroceryItem(item)}
+              onPress={() => updateGroceryItem(item)}
+            />
+          )}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={styles.sectionHeader}>{title}</Text>
+          )}
+          keyExtractor={(item) => item._id.toString()}
+        />
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
   groceryContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 8,
-      width: '100%',
-      backgroundColor: '#000',
-      marginBottom: 16,
-      padding: 8,
-      borderTopColor: 'white',
-      borderTopWidth: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    width: '100%',
+    backgroundColor: '#000',
+    marginBottom: 16,
+    padding: 8,
+    borderTopColor: 'white',
+    borderTopWidth: 2,
   },
   header: {
-      backgroundColor: 'black',
-      height: 80,
+    backgroundColor: 'black',
+    height: 80,
   },
   sectionHeader: {
-      color: '#505050',
-      fontSize: 18,
-      fontWeight: 'bold',
-      padding: 8,
+    color: '#505050',
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 8,
   },
   headerText: {
-      color: 'white',
-      fontSize: 24,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      lineHeight: 80,
-      paddingTop: Platform.OS === 'ios' ? 16 : 0,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: 80,
+    paddingTop: Platform.OS === 'ios' ? 16 : 0,
   },
   nameContainer: {
-      gap: 8,
-      width: '87%',
-      borderColor: 'gray',
-      borderWidth: 0.5,
-      height: 40,
-      borderRadius: 8,
-      backgroundColor: '#FFFFFF',
-      padding: 8,
-  },
-  quantityContainer: {
-      gap: 8,
-      flex: 2,
-      borderColor: 'gray',
-      borderWidth: 0.5,
-      height: 40,
-      borderRadius: 8,
-      padding: 8,
-      backgroundColor: '#FFFFFF',
+    gap: 8,
+    width: '87%',
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    padding: 8,
   },
   screen: {
-      flex: 1,
-      backgroundColor: '#FFFFFF',
-  },
-  reactLogo: {
-      paddingTop: 32,
-      height: 200,
-      width: '100%',
+    flex: 1,
+    backgroundColor: '#FFFFFF',
   },
   plusIcon: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 48,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
   }
 });
--- components/GroceryItem.tsx
+++ components/GroceryItem.tsx
@@ -4,49 +4,49 @@ import Fontisto from '@expo/vector-icons/Fontisto';
 import { GroceryItem } from '@/models/GroceryItem';
 
 interface GroceryListItemProps {
-    item: GroceryItem;
-    onPress: () => void;
-    onDelete: () => void;
+  item: GroceryItem;
+  onPress: () => void;
+  onDelete: () => void;
 }
 
 export function GroceryListItem( { item, onPress, onDelete }: GroceryListItemProps) {
-    return (
-        <Pressable style={styles.groceryItemContainer} onLongPress={onDelete}>
-            <Text
-                style={styles.groceryItemText}
-            >{item.name}</Text>
-            <Pressable 
-                onPress={onPress} 
-                style={styles.checkboxContainer}
-            >
-                <Fontisto name={item.isPurchased ? "checkbox-active" : "checkbox-passive"} size={24} color="#CCCCCC" />
-            </Pressable>
-        </Pressable>
-    );
+  return (
+    <Pressable style={styles.groceryItemContainer} onLongPress={onDelete}>
+      <Text
+        style={styles.groceryItemText}
+      >{item.name}</Text>
+      <Pressable 
+        onPress={onPress} 
+        style={styles.checkboxContainer}
+      >
+        <Fontisto name={item.isPurchased ? "checkbox-active" : "checkbox-passive"} size={24} color="#CCCCCC" />
+      </Pressable>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    groceryItemContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        borderColor: '#CCCCCC',
-        borderWidth: 0.5,
-        borderRadius: 4,
-        marginBottom: 8,
-        marginHorizontal: 8,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 4,
-    },
-    groceryItemText: {
-        color: 'black',
-        fontSize: 16,
-        padding: 8,
-    },
-    checkboxContainer: {
-        width: 40,
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  groceryItemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderColor: '#CCCCCC',
+    borderWidth: 0.5,
+    borderRadius: 4,
+    marginBottom: 8,
+    marginHorizontal: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 4,
+  },
+  groceryItemText: {
+    color: 'black',
+    fontSize: 16,
+    padding: 8,
+  },
+  checkboxContainer: {
+    width: 40,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
